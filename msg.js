51dfaeb65d4b6127e481cea6bfb4d5df65f73638
dokja124 <malandaniel250@gmail.com>
2026-08-06 09:47:06 +0000

    Update msg.js

--- msg.js
+++ msg.js
@@ -246,5 +246,5 @@ const sms = (conn, m) => {
 module.exports = {
     sms,
     downloadMediaMessage
-                                 }
+    }                             
                                      
