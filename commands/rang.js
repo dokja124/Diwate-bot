e81e3e7c7820e90d311ff111c1fc305a85437302
dokja124 <malandaniel250@gmail.com>
2026-08-09 08:57:27 +0000

    Update rang.js

--- commands/rang.js
+++ commands/rang.js
@@ -225,7 +225,7 @@ async function handleRang(socket, msg, sender, isGroup, nowsender, args, fakevCa
         }
 
         if (ppUrl) {
-            await socket.sendMessage(sender,{ react: { text: '✨', key: msg.key } }).catch(() => {}); {
+            await socket.sendMessage(sender, {
                 image: { url: ppUrl },
                 caption,
                 mentions: [target]
